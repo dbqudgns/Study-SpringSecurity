a0a4c5db8ef2d96d8c41d482736f0df59cbda166
dbqudgns <dbqudgns4632@gmail.com>
2025-02-27 16:45:36 +0900

    my-app 속 .git 제거

---
+++ OAuth2JWT/src/main/my-app/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+const onNaverLogin = () => {
+  window.location.href = "http://localhost:8080/oauth2/authorization/naver"
+}
+
+const onGoogleLogin = () => {
+    window.location.href = "http://localhost:8080/oauth2/authorization/google"
+}
+
+const getData = () => {
+
+  fetch("http://localhost:8080/my", {
+    method: "GET",
+    credentials: 'include'
+  })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(data)
+      })
+      .catch((error) => alert(error))
+}
+
+function App() {
+  return (
+      <>
+          <button onClick={onNaverLogin}>NAVER LOGIN</button>
+          <button onClick={onGoogleLogin}>GOOGLE LOGIN</button>
+          <button onClick={getData}>GET DATA</button>
+      </>
+  );
+}
+
+export default App;
